17c10d53041f6b95d602d93f7c7ae84a74523f65
rafaelglikis <rafaelglikis@gmail.com>
2026-05-12 23:28:22 +0300

    Update lazyvim config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Tmux-style splits
+vim.keymap.set("n", '<leader>"', ":split<CR>", { desc = "Split window below" })
+vim.keymap.set("n", "<leader>%", ":vsplit<CR>", { desc = "Split window right" })
+
+-- Remove LazyVim default split bindings
+pcall(vim.keymap.del, "n", "<leader>-")
+pcall(vim.keymap.del, "n", "<leader>|")
+
+-- Diff (git hunk) navigation
+local function next_hunk() require("gitsigns").nav_hunk("next") end
+local function prev_hunk() require("gitsigns").nav_hunk("prev") end
+vim.keymap.set("n", "]d", next_hunk, { desc = "Next diff" })
+vim.keymap.set("n", "[d", prev_hunk, { desc = "Previous diff" })
+vim.keymap.set("n", "]c", next_hunk, { desc = "Next diff" })
+vim.keymap.set("n", "[c", prev_hunk, { desc = "Previous diff" })
+
+-- Diagnostic navigation
+vim.keymap.set("n", "]e", function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Next diagnostic" })
+vim.keymap.set("n", "[e", function() vim.diagnostic.jump({ count = -1 }) end, { desc = "Previous diagnostic" })
+
+-- Revert git hunk
+vim.keymap.set("n", "<leader>gr", function() require("gitsigns").reset_hunk() end, { desc = "Revert git hunk" })
+
+-- Reload config
+vim.keymap.set("n", "<leader><CR>", ":source $MYVIMRC<CR>", { desc = "Reload config" })
---
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      { "<leader>-", hidden = true },
+      { "<leader>|", hidden = true },
+    },
+  },
+}
